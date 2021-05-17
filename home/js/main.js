b8c3771b330868b5acbd88b8d4b328a8ef67a138
woqhr153@naver.com <woqhr153@naver.com>
2021-05-17 12:49:47 +0900

    오전 커밋

--- home/js/main.js
+++ home/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
     $('.to_top a').smoothScroll();
+    
+    //모바일용 메뉴 보이기/숨기기 액션처리
+		$('.openMOgnb').click(function(){
+            // alert('메뉴보이기 클릭액션');
+            // $('.header_cont').css('display','block');
+            $('.header_cont').slideDown("slow");
+            $("#header").addClass("on");
+        });
+        $('.closePop').click(function(){
+            // $('.header_cont').css('display','none');
+            $('.header_cont').slideUp("fast");
+            $('#header').removeClass('on');
+        });
 });
